94a9d276e0db26099218e1c64ea64fc07fffaf4f
Alexander Bruy <alexander.bruy@gmail.com>
2010-02-04 17:30:42 +0000

    fixed bug with setting up QTranslator when translation file is not exists relocated check for matplotlib

--- statist.py
+++ statist.py
@@ -39,77 +39,88 @@ import doStatist, doAbout, utils
 import resources
 
 class statistPlugin:
-	def __init__( self, iface ):
-		self.iface = iface
-		try:
-			self.QgisVersion = unicode( QGis.QGIS_VERSION_INT )
-		except:
-			self.QgisVersion = unicode( QGis.qgisVersion )[ 0 ]
-		
-		userPluginPath = QFileInfo( QgsApplication.qgisUserDbFilePath() ).path() + "/python/plugins/statist"
-		systemPluginPath = QgsApplication.prefixPath() + "/python/plugins/statist"
-		# install translator for i18n
-		localeFullName = QLocale.system().name()
-		
-		if QFileInfo( userPluginPath ).exists():
-			translatePath = userPluginPath + "/i18n/statist_" + localeFullName + ".qm"
-		else:
-			translatePath = systemPluginPath + "/i18n/statist_" + localeFullName + ".qm"
-		
-		self.localePath = translatePath
-		self.translator = QTranslator()
-		self.translator.load( self.localePath )
-		if qVersion() > '4.3.3':
-			QCoreApplication.installTranslator( self.translator )
-		
-		# static messages
-		self.wrongVersion = QCoreApplication.translate( "Statist", "Quantum GIS version detected: " +
-		self.QgisVersion + "\n" + "Statist plugin requires version at least 1.0.0!\n" +
-		"Plugin not loaded." )
-		
-		self.aboutString = QCoreApplication.translate( "Statist", "Statist\nQuantum GIS Python plugin\n" +
-		"The plugin calculate statistics on selected field of vector layer\n\n" +
-		"Author: Alexander Bruy\n" + "Mail: alexander.bruy@gmail.com" )
-		
-		self.noLayers = QCoreApplication.translate( "Statist", "Plugin will not run, because there is\n" +
-		"no vector layers in this project" )
-	
-	def initGui( self ):
-		# check Qgis version
-		if int( self.QgisVersion ) < 1:
-			QMessageBox.warning( self.iface.mainWindow(), "Statist: Error", self.wrongVersion )
-			return None
-		
-		# create plugin menu
-		self.calcStats = QAction( QCoreApplication.translate( "mnuStatist", "Statistics" ), self.iface.mainWindow() )
-		self.calcStats.setIcon( QIcon( ":/icons/statist.png" ) )
-		self.calcStats.setWhatsThis( "Calculate statistics" )
-		self.aboutStatist = QAction( QCoreApplication.translate( "mnuStatist", "About..." ), self.iface.mainWindow() )
-		self.aboutStatist.setIcon( QIcon( ":/icons/information.png" ) )
-		self.aboutStatist.setWhatsThis( "About Statist plugin" )
-		self.iface.addPluginToMenu( "Statist", self.calcStats )
-		self.iface.addPluginToMenu( "Statist", self.aboutStatist )
-		
-		# add toolbar icon
-		self.iface.addToolBarIcon( self.calcStats )
-		
-		QObject.connect( self.calcStats, SIGNAL( "triggered()" ), self.doCalcStats )
-		QObject.connect( self.aboutStatist, SIGNAL( "triggered()" ), self.showAbout )
-	
-	def unload( self ):
-		self.iface.removeToolBarIcon( self.calcStats )
-		self.iface.removePluginMenu( "Statist", self.calcStats )
-		self.iface.removePluginMenu( "Statist", self.aboutStatist )
-	
-	def showAbout( self ):
-		#QMessageBox.information( self.iface.mainWindow(), self.tr( "About Statist" ), self.aboutString )
-		d = doAbout.dlgAbout( self.iface )
-		d.exec_()
-	
-	def doCalcStats( self ):
-		nLayers = len( utils.getLayersNames( "vector" ) )
-		if nLayers == 0:
-			QMessageBox.warning( self.iface.mainWindow(), "Statist: Error", self.noLayers )
-			return None
-		d = doStatist.dlgStatist( self.iface )
-		d.exec_()
+  def __init__( self, iface ):
+    self.iface = iface
+    try:
+      self.QgisVersion = unicode( QGis.QGIS_VERSION_INT )
+    except:
+      self.QgisVersion = unicode( QGis.qgisVersion )[ 0 ]
+
+    userPluginPath = QFileInfo( QgsApplication.qgisUserDbFilePath() ).path() + "/python/plugins/statist"
+    systemPluginPath = QgsApplication.prefixPath() + "/python/plugins/statist"
+    # install translator for i18n
+    localeFullName = QLocale.system().name()
+
+    if QFileInfo( userPluginPath ).exists():
+      translatePath = userPluginPath + "/i18n/statist_" + localeFullName + ".qm"
+    else:
+      translatePath = systemPluginPath + "/i18n/statist_" + localeFullName + ".qm"
+
+    self.localePath = translatePath
+    if QFileInfo( self.localePath ).exists():
+      self.translator = QTranslator()
+      self.translator.load( self.localePath )
+      QCoreApplication.installTranslator( self.translator )
+
+    # static messages
+    self.wrongVersion = QCoreApplication.translate( "Statist", "Quantum GIS version detected: " +
+    self.QgisVersion + "\n" + "Statist plugin requires version at least 1.0.0!\n" +
+      "Plugin not loaded." )
+
+    self.msgError = QCoreApplication.translate( "Statist", "Couldn't import Python module 'matplotlib' for plotting. Without it you won't be able to run Statist." )
+
+    self.aboutString = QCoreApplication.translate( "Statist", "Statist\nQuantum GIS Python plugin\n" +
+      "The plugin calculate statistics on selected field of vector layer\n\n" +
+      "Author: Alexander Bruy\n" + "Mail: alexander.bruy@gmail.com" )
+
+    self.noLayers = QCoreApplication.translate( "Statist", "Plugin will not run, because there is\n" +
+      "no vector layers in this project" )
+
+  def initGui( self ):
+    # check Qgis version
+    if int( self.QgisVersion ) < 1:
+      QMessageBox.warning( self.iface.mainWindow(), "Statist: Error", self.wrongVersion )
+      return None
+
+    # create plugin menu
+    self.calcStats = QAction( QCoreApplication.translate( "mnuStatist", "Statistics" ), self.iface.mainWindow() )
+    self.calcStats.setIcon( QIcon( ":/icons/statist.png" ) )
+    self.calcStats.setWhatsThis( "Calculate statistics" )
+    self.aboutStatist = QAction( QCoreApplication.translate( "mnuStatist", "About..." ), self.iface.mainWindow() )
+    self.aboutStatist.setIcon( QIcon( ":/icons/information.png" ) )
+    self.aboutStatist.setWhatsThis( "About Statist plugin" )
+    self.iface.addPluginToMenu( "Statist", self.calcStats )
+    self.iface.addPluginToMenu( "Statist", self.aboutStatist )
+
+    # add toolbar icon
+    self.iface.addToolBarIcon( self.calcStats )
+
+    QObject.connect( self.calcStats, SIGNAL( "triggered()" ), self.doCalcStats )
+    QObject.connect( self.aboutStatist, SIGNAL( "triggered()" ), self.showAbout )
+
+  def unload( self ):
+    self.iface.removeToolBarIcon( self.calcStats )
+    self.iface.removePluginMenu( "Statist", self.calcStats )
+    self.iface.removePluginMenu( "Statist", self.aboutStatist )
+
+  def showAbout( self ):
+    #QMessageBox.information( self.iface.mainWindow(), self.tr( "About Statist" ), self.aboutString )
+    d = doAbout.dlgAbout( self.iface )
+    d.exec_()
+
+  def doCalcStats( self ):
+    try:
+      from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+      from matplotlib.backends.backend_qt4agg import NavigationToolbar2QTAgg as NavigationToolbar
+      from matplotlib.figure import Figure
+      import matplotlib.font_manager as FontManager
+    except ImportError:
+      QMessageBox.warning( None, "Statist: Error", msgError )
+      return None
+
+    nLayers = len( utils.getLayersNames( "vector" ) )
+    if nLayers == 0:
+      QMessageBox.warning( self.iface.mainWindow(), "Statist: Error", self.noLayers )
+      return None
+    d = doStatist.dlgStatist( self.iface )
+    d.exec_()
